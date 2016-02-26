78b5e463e0819a59d5d4d8ac4a3163f01a7a4918
Artem Rizhenkov <riartem@gmail.com>
2016-02-26 14:24:40 +0300

    Version 1.0
    - Added ability to place key in secret.txt file
    - Added timeout setting and some curl options for speed up sending
    - Removed mirror VDB (not all patterns allow place two classes into one file. e.g. VDB was might be unavailable in some autoload configurations)

--- VarDumpBot.php
+++ VarDumpBot.php
@@ -4,48 +4,41 @@ class VarDumpBot
 {
     protected static $project_key = '';
     protected static $service_url = 'https://debug.tbot.me/ping';
-
+    // Decrease $timeout for speed up sending info. But if messages not delivered means value is too low.
+    protected static $timeout = 200;
     public static function log($something_here)
     {
         $result = false;
+        if(self::$project_key == '' and file_exists(__DIR__.'/secret.txt')){
+            self::$project_key = trim(@file_get_contents(__DIR__.'/secret.txt'));
+        }
+        if(self::$project_key == '') return 'empty project key';
 
         $content = @json_encode($something_here);
-        if($content === false) return $result;
-
+        if($content === false) return 'error encoding content';
         if(self::curl($content) !== false){
             $result = true;
         }
-
         return $result;
     }
-
     protected static function curl($content)
     {
         if(! function_exists('curl_init')){
             return false;
         }
-
         $ch = @curl_init(self::$service_url);
         if($ch === false) return false;
-
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, array(
             'key'  => self::$project_key,
             'data' => $content
         ));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $response = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, self::$timeout);
+        curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
+        curl_exec($ch);
         curl_close($ch);
-        if($response === false) return false;
         return true;
-    }
-}
-
-
-class VDB {
-    public static function log($s_h)
-    {
-        return VarDumpBot::log($s_h);
     }
 }
